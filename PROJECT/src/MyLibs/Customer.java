0347cf76f6e820b8bb7154e38f5a2547e6706b5e
M-igzy <88198129+M-igzy@users.noreply.github.com>
2021-07-30 22:01:49 +0800

    Add files via upload

---
+++ PROJECT/src/MyLibs/Customer.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyLibs;
+
+/**
+ *
+ * @author Carlo Miguel Legaspi
+ */
+public class Customer {
+    
+            private String CustomerID;
+            private String Fname;
+            private String Sname;
+
+    public String getCustomerID() {
+        return CustomerID;
+    }
+
+    public void setCustomerID(String CustomerID) {
+        this.CustomerID = CustomerID;
+    }
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String Fname) {
+        this.Fname = Fname;
+    }
+
+    public String getSname() {
+        return Sname;
+    }
+
+    public void setSname(String Sname) {
+        this.Sname = Sname;
+    }
+    
+}
---
+++ PROJECT/src/MyLibs/Property.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyLibs;
+
+/**
+ *
+ * @author Carlo Miguel Legaspi
+ */
+public class Property {
+    
+    private int block;
+    private int lot;
+    private Double cost;
+    private Double sqkm;
+
+    public int getBlock() {
+        return block;
+    }
+
+    public void setBlock(int block) {
+        this.block = block;
+    }
+
+    public int getLot() {
+        return lot;
+    }
+
+    public void setLot(int lot) {
+        this.lot = lot;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Double getSqkm() {
+        return sqkm;
+    }
+
+    public void setSqkm(Double sqkm) {
+        this.sqkm = sqkm;
+    }
+    
+}
